a601447913e7f277fc44a017db9ed565b34a45a0
kleqing <hotaruu24k@gmail.com>
2025-01-08 10:32:37 +0700

    Camera: Follow player instead of fixed

--- Assets/Script/Core/CameraController.cs
+++ Assets/Script/Core/CameraController.cs
@@ -2,24 +2,34 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [Header("Player Camera")]
-    [SerializeField] private Transform player;
-    [SerializeField] private float cameraAhead;
-
+    //* Fixed camera
     [Header("Room Transition")] 
     [SerializeField] private float cameraSpeed;
     
     private Vector3 velocity = Vector3.zero;
     private float currentPositonY;
+    
+    //* Camera follow player
+    [Header("Player Camera")]
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    
+    private float lookAheadX;
     private float lookAheadY;
     
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, 
+        //* Camera transition between rooms
+        /* transform.position = Vector3.SmoothDamp(transform.position, 
             new Vector3(transform.position.x, currentPositonY, transform.position.z), 
             ref velocity, 
-            cameraSpeed * Time.deltaTime);
-        lookAheadY = Mathf.Lerp(lookAheadY, (cameraAhead * transform.localScale.y), Time.deltaTime * cameraSpeed);
+            cameraSpeed * Time.deltaTime); */
+        
+        //* Camera follow player
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        lookAheadX = Mathf.Lerp(lookAheadX, (aheadDistance * transform.localScale.x), Time.deltaTime * cameraSpeed);
+        lookAheadY = Mathf.Lerp(lookAheadY, (aheadDistance * transform.localScale.y), Time.deltaTime * cameraSpeed);
+
     }
 
     public void MoveToNextRoom(Transform nextRoom)
